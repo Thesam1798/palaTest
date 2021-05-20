b1232bc44ae1bf0974af634a917709be1df11215
Thesam1798 <debris.alex@gmail.com>
2021-05-21 00:45:58 +0200

    Version Upgrade To 2021.5.2

--- src/main/java/fr/debris/palatest/common/Reference.java
+++ src/main/java/fr/debris/palatest/common/Reference.java
@@ -10,7 +10,7 @@ package fr.debris.palatest.common;
 public class Reference {
     public static final String MOD_NAME = "Pala Test";
     public static final String MOD_ID = "palatest";
-    public static final String MOD_VERSION = "2021.5.1";
+    public static final String MOD_VERSION = "2021.5.2";
 
     public static final String PROXY_CLIENT_CLASS = "fr.debris.palatest.client.proxy.ClientProxy";
     public static final String PROXY_SERVER_CLASS = "fr.debris.palatest.common.proxy.CommonProxy";
